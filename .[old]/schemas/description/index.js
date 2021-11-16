33d6c5dc79630288370f444a65be205b7b0d7dcd
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-11-16 13:37:40 +0100

    yea

---
+++ .[old]/schemas/description/index.js
@@ -0,0 +1,9 @@
+const descriptionDefinition = {
+    length: {
+        min: 0,
+        max: 320,
+    },
+    default: ""
+};
+
+module.exports = descriptionSchema;
---
+++ .[old]/schemas/username/_test.js
@@ -0,0 +1,2 @@
+const usernameSchema = require('.');
+
