97afa3549ef8bc0336a5b4fbad7d4275c11aa74f
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2023-05-01 16:02:21 +0200

    tsconfig update

--- src/core_types/integer.ts
+++ src/core_types/integer.ts
@@ -1,1 +1,5 @@
-module.exports = async (value) => (await require('./number')(value) && (-2147483649 < value && value < 2147483648) && value % 1 === 0);
+const isNumber = require('./number');
+
+const isInteger = async (value : any) => (await isNumber(value) && (-2147483649 < value && value < 2147483648) && value % 1 === 0);
+
+module.exports = isInteger;
